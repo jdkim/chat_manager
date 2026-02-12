dff6f8bcb4c8a49fcb65499425c42223352430b0
dhq_boiler <dhq_boiler@live.jp>
2026-02-12 15:02:06 +0900

    feat: add CSV download UI to chat list
    Add per-chat download button and download-all link to the chat list view.
    Update helpers to accept download_csv_path and download_all_csv_path parameters.

--- lib/chat_manager/helpers.rb
+++ lib/chat_manager/helpers.rb
@@ -1,7 +1,7 @@
 module ChatManager
   module Helpers
-    def chat_list(card_path, active_uuid: nil)
-      render "chat_manager/chat_list", locals: { card_path: card_path, active_uuid: active_uuid }
+    def chat_list(card_path, active_uuid: nil, download_csv_path: nil, download_all_csv_path: nil)
+      render "chat_manager/chat_list", locals: { card_path: card_path, active_uuid: active_uuid, download_csv_path: download_csv_path, download_all_csv_path: download_all_csv_path }
     end
   end
 end
